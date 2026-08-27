08b23f14a11e271ae00851047a904e90dff583c6
Qi Gao <qi.gao2012@gmail.com>
2026-08-27 19:21:14 +0800

    feat: add render rotation option

--- include/extractpdf/extractpdf.h
+++ include/extractpdf/extractpdf.h
@@ -36,6 +36,7 @@ typedef enum extractpdf_page_box {
 typedef struct extractpdf_render_options {
     size_t struct_size;
     float dpi;
+    float rotation_degrees;
 } extractpdf_render_options;
 
 typedef enum extractpdf_status {
