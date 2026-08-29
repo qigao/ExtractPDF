94362bf6b96ae1d751e9925279146c44ef140c6a
Qi Gao <qi.gao2012@gmail.com>
2026-08-29 19:10:04 +0800

    test: expose CropBox default-box assertion

--- tests/test_pdf_crop_internal.h
+++ tests/test_pdf_crop_internal.h
@@ -10,6 +10,11 @@ int crop_raw_expect_local_cropbox(
     int expect_present,
     const float expected[4]);
 
+int crop_raw_expect_no_local_default_boxes(
+    const unsigned char *data,
+    size_t size,
+    int page_index);
+
 int crop_raw_expect_preserved_graph(
     const unsigned char *before,
     size_t before_size,
