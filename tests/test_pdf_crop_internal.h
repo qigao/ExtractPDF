2bfc4439fb9c21358c105ce0fa778772a74a1c1b
Qi Gao <qi.gao2012@gmail.com>
2026-08-29 18:23:26 +0800

    test: add CropBox raw helper interface

---
+++ tests/test_pdf_crop_internal.h
@@ -0,0 +1,19 @@
+#ifndef EXTRACTPDF_TEST_PDF_CROP_INTERNAL_H
+#define EXTRACTPDF_TEST_PDF_CROP_INTERNAL_H
+
+#include <stddef.h>
+
+int crop_raw_expect_local_cropbox(
+    const unsigned char *data,
+    size_t size,
+    int page_index,
+    int expect_present,
+    const float expected[4]);
+
+int crop_raw_expect_preserved_graph(
+    const unsigned char *before,
+    size_t before_size,
+    const unsigned char *after,
+    size_t after_size);
+
+#endif
