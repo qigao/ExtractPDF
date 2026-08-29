ea5f68edb4ba20f513aa8896e3258291004af5e0
Qi Gao <qi.gao2012@gmail.com>
2026-08-29 23:01:00 +0800

    test: lock transformed MediaBox trim policy

--- tests/test_pdf_trim_internal.h
+++ tests/test_pdf_trim_internal.h
@@ -23,6 +23,18 @@ int trim_raw_expect_preserved_graph(
     const unsigned char *after,
     size_t after_size);
 
+int trim_raw_expect_production_boxes(
+    const unsigned char *data,
+    size_t size,
+    int page_index,
+    int expect_bleed,
+    const float bleed[4],
+    int expect_trim,
+    const float trim[4],
+    int expect_art,
+    const float art[4]);
+
 int trim_run_frame_mode_tests(void);
+int trim_run_policy_tests(void);
 
 #endif
