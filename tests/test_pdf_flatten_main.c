43955b164d6b60bd4c3fe2f4145713514af7127c
Qi Gao <qi.gao2012@gmail.com>
2026-08-31 07:52:15 +0800

    test: wire Link default-border Flatten RED

--- tests/test_pdf_flatten_main.c
+++ tests/test_pdf_flatten_main.c
@@ -5,6 +5,7 @@ int extractpdf_test_pdf_flatten_form_multi(void);
 int extractpdf_test_pdf_flatten_form_closure(void);
 int extractpdf_test_pdf_flatten_form_ancestor_survives(void);
 int extractpdf_test_pdf_flatten_widget_as(void);
+int extractpdf_test_pdf_flatten_policy(void);
 
 int main(void)
 {
@@ -21,6 +22,8 @@ int main(void)
     if (extractpdf_test_pdf_flatten_form_ancestor_survives() != 0)
         return 1;
     if (extractpdf_test_pdf_flatten_widget_as() != 0)
+        return 1;
+    if (extractpdf_test_pdf_flatten_policy() != 0)
         return 1;
     return extractpdf_pdf_flatten_base_main();
 }
