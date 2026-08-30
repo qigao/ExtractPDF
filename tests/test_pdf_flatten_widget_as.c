8173dc797886238b15b4dbeb1ce4af6d23ba8203
Qi Gao <qi.gao2012@gmail.com>
2026-08-31 07:47:33 +0800

    test: verify radio Widgets follow AS state

--- tests/test_pdf_flatten_widget_as.c
+++ tests/test_pdf_flatten_widget_as.c
@@ -21,7 +21,10 @@ static void raw_check_impl(int ok, const char *expr, int line)
 }
 #define RAW_CHECK(x) raw_check_impl((x), #x, __LINE__)
 
-static int check_source_form(extractpdf_document *document)
+static int check_source_form(
+    extractpdf_document *document,
+    extractpdf_form_field_type expected_type,
+    size_t expected_widgets)
 {
     extractpdf_form *form = NULL;
     extractpdf_form_field_info field = {0};
@@ -34,9 +37,9 @@ static int check_source_form(extractpdf_document *document)
     CHECK(extractpdf_form_field_count(form, &field_count) == EXTRACTPDF_OK);
     CHECK(field_count == 1);
     CHECK(extractpdf_form_widget_count(form, &widget_count) == EXTRACTPDF_OK);
-    CHECK(widget_count == 1);
+    CHECK(widget_count == expected_widgets);
     CHECK(extractpdf_form_field_get_info(form, 0, &field) == EXTRACTPDF_OK);
-    CHECK(field.type == EXTRACTPDF_FORM_FIELD_CHECKBOX);
+    CHECK(field.type == expected_type);
     extractpdf_drop_form(form);
     return 0;
 }
@@ -72,7 +75,23 @@ static pdf_document *open_output_pdf(
     return document;
 }
 
-static void check_as_selected_output(const extractpdf_output *output)
+static void check_marker(
+    fz_context *ctx,
+    pdf_obj *xobjects,
+    const char *alias,
+    pdf_obj *expected_marker)
+{
+    pdf_obj *appearance = pdf_dict_gets(ctx, xobjects, alias);
+    pdf_obj *marker;
+
+    RAW_CHECK(pdf_is_indirect(ctx, appearance));
+    RAW_CHECK(pdf_is_stream(ctx, appearance));
+    marker = pdf_dict_gets(ctx, appearance, "StateMarker");
+    RAW_CHECK(pdf_is_name(ctx, marker));
+    RAW_CHECK(pdf_name_eq(ctx, marker, expected_marker));
+}
+
+static void check_checkbox_output(const extractpdf_output *output)
 {
     fz_context *ctx = fz_new_context(NULL, NULL, FZ_STORE_DEFAULT);
     pdf_document *document = NULL;
@@ -80,8 +99,6 @@ static void check_as_selected_output(const extractpdf_output *output)
     pdf_obj *page;
     pdf_obj *resources;
     pdf_obj *xobjects;
-    pdf_obj *appearance;
-    pdf_obj *marker;
     int caught_code = FZ_ERROR_NONE;
 
     RAW_CHECK(ctx != NULL);
@@ -102,12 +119,7 @@ static void check_as_selected_output(const extractpdf_output *output)
         RAW_CHECK(pdf_is_dict(ctx, resources));
         xobjects = pdf_dict_get(ctx, resources, PDF_NAME(XObject));
         RAW_CHECK(pdf_is_dict(ctx, xobjects));
-        appearance = pdf_dict_gets(ctx, xobjects, "EPB0");
-        RAW_CHECK(pdf_is_indirect(ctx, appearance));
-        RAW_CHECK(pdf_is_stream(ctx, appearance));
-        marker = pdf_dict_gets(ctx, appearance, "StateMarker");
-        RAW_CHECK(pdf_is_name(ctx, marker));
-        RAW_CHECK(pdf_name_eq(ctx, marker, PDF_NAME(Yes)));
+        check_marker(ctx, xobjects, "EPB0", PDF_NAME(Yes));
     }
     fz_always(ctx)
     {
@@ -123,24 +135,99 @@ static void check_as_selected_output(const extractpdf_output *output)
     RAW_CHECK(caught_code == FZ_ERROR_NONE);
 }
 
-int extractpdf_test_pdf_flatten_widget_as(void)
+static void check_radio_output(const extractpdf_output *output)
+{
+    fz_context *ctx = fz_new_context(NULL, NULL, FZ_STORE_DEFAULT);
+    pdf_document *document = NULL;
+    pdf_obj *root;
+    pdf_obj *page;
+    pdf_obj *resources;
+    pdf_obj *xobjects;
+    int caught_code = FZ_ERROR_NONE;
+
+    RAW_CHECK(ctx != NULL);
+    fz_var(document);
+    fz_var(caught_code);
+    fz_try(ctx)
+    {
+        document = open_output_pdf(ctx, output);
+        RAW_CHECK(document != NULL);
+        root = pdf_dict_get(ctx, pdf_trailer(ctx, document), PDF_NAME(Root));
+        RAW_CHECK(pdf_is_dict(ctx, root));
+        RAW_CHECK(pdf_dict_get(ctx, root, PDF_NAME(AcroForm)) == NULL);
+
+        page = pdf_lookup_page_obj(ctx, document, 0);
+        RAW_CHECK(pdf_is_dict(ctx, page));
+        RAW_CHECK(pdf_dict_get(ctx, page, PDF_NAME(Annots)) == NULL);
+        resources = pdf_dict_get(ctx, page, PDF_NAME(Resources));
+        RAW_CHECK(pdf_is_dict(ctx, resources));
+        xobjects = pdf_dict_get(ctx, resources, PDF_NAME(XObject));
+        RAW_CHECK(pdf_is_dict(ctx, xobjects));
+        check_marker(ctx, xobjects, "EPB0", PDF_NAME(OffA));
+        check_marker(ctx, xobjects, "EPB1", PDF_NAME(B));
+    }
+    fz_always(ctx)
+    {
+        pdf_drop_document(ctx, document);
+        document = NULL;
+    }
+    fz_catch(ctx)
+    {
+        caught_code = fz_caught(ctx);
+        fz_report_error(ctx);
+    }
+    fz_drop_context(ctx);
+    RAW_CHECK(caught_code == FZ_ERROR_NONE);
+}
+
+static int check_checkbox_as(void)
 {
     extractpdf_document *document = NULL;
     extractpdf_output *output = NULL;
 
     CHECK(extractpdf_open(FLATTEN_WIDGET_AS_PDF, NULL, &document) == EXTRACTPDF_OK);
     CHECK(document != NULL);
-    CHECK(check_source_form(document) == 0);
+    CHECK(check_source_form(document, EXTRACTPDF_FORM_FIELD_CHECKBOX, 1) == 0);
 
     CHECK(extractpdf_flatten_interactive(
         document,
         EXTRACTPDF_FLATTEN_WIDGETS,
         &output) == EXTRACTPDF_OK);
     CHECK(output != NULL);
-    check_as_selected_output(output);
+    check_checkbox_output(output);
 
-    CHECK(check_source_form(document) == 0);
+    CHECK(check_source_form(document, EXTRACTPDF_FORM_FIELD_CHECKBOX, 1) == 0);
     extractpdf_drop_output(output);
     extractpdf_close(document);
     return 0;
+}
+
+static int check_radio_as(void)
+{
+    extractpdf_document *document = NULL;
+    extractpdf_output *output = NULL;
+
+    CHECK(extractpdf_open(
+        FLATTEN_WIDGET_RADIO_AS_PDF, NULL, &document) == EXTRACTPDF_OK);
+    CHECK(document != NULL);
+    CHECK(check_source_form(document, EXTRACTPDF_FORM_FIELD_RADIO_BUTTON, 2) == 0);
+
+    CHECK(extractpdf_flatten_interactive(
+        document,
+        EXTRACTPDF_FLATTEN_WIDGETS,
+        &output) == EXTRACTPDF_OK);
+    CHECK(output != NULL);
+    check_radio_output(output);
+
+    CHECK(check_source_form(document, EXTRACTPDF_FORM_FIELD_RADIO_BUTTON, 2) == 0);
+    extractpdf_drop_output(output);
+    extractpdf_close(document);
+    return 0;
+}
+
+int extractpdf_test_pdf_flatten_widget_as(void)
+{
+    if (check_checkbox_as() != 0)
+        return 1;
+    return check_radio_as();
 }
