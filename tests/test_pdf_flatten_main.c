35d08f3aada8c37d904caef90ca3ab9b2453204b
Qi Gao <qi.gao2012@gmail.com>
2026-08-30 23:35:44 +0800

    test: wire affected field closure RED

--- tests/test_pdf_flatten_main.c
+++ tests/test_pdf_flatten_main.c
@@ -2,6 +2,7 @@
 
 int extractpdf_pdf_flatten_base_main(void);
 int extractpdf_test_pdf_flatten_form_multi(void);
+int extractpdf_test_pdf_flatten_form_closure(void);
 
 int main(void)
 {
@@ -12,6 +13,8 @@ int main(void)
     if (extractpdf_test_pdf_flatten_form() != 0)
         return 1;
     if (extractpdf_test_pdf_flatten_form_multi() != 0)
+        return 1;
+    if (extractpdf_test_pdf_flatten_form_closure() != 0)
         return 1;
     return extractpdf_pdf_flatten_base_main();
 }
