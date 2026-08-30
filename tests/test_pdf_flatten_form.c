a9d1e4ea657cb1e3436220fe30bd93ffbaa3c3ab
Qi Gao <qi.gao2012@gmail.com>
2026-08-30 18:59:07 +0800

    test: isolate Widget flatten RED boundary

--- tests/test_pdf_flatten_form.c
+++ tests/test_pdf_flatten_form.c
@@ -14,14 +14,27 @@
 int extractpdf_test_pdf_flatten_form(void)
 {
     extractpdf_document *document = NULL;
+    extractpdf_form *form = NULL;
     extractpdf_output *output = NULL;
+    extractpdf_status status;
+    size_t widget_count = 0;
 
     CHECK(extractpdf_open(FLATTEN_WIDGETS_PDF, NULL, &document) == EXTRACTPDF_OK);
     CHECK(document != NULL);
-    CHECK(extractpdf_flatten_interactive(
+
+    CHECK(extractpdf_document_form(document, &form) == EXTRACTPDF_OK);
+    CHECK(form != NULL);
+    CHECK(extractpdf_form_widget_count(form, &widget_count) == EXTRACTPDF_OK);
+    CHECK(widget_count == 7);
+    extractpdf_drop_form(form);
+    form = NULL;
+
+    status = extractpdf_flatten_interactive(
         document,
         EXTRACTPDF_FLATTEN_WIDGETS,
-        &output) == EXTRACTPDF_OK);
+        &output);
+    fprintf(stderr, "widget flatten status=%d\n", (int)status);
+    CHECK(status == EXTRACTPDF_OK);
     CHECK(output != NULL);
 
     extractpdf_drop_output(output);
