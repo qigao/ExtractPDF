7ab77c1f88d9f8c01829fa23c50d1419cf581774
Qi Gao <qi.gao2012@gmail.com>
2026-08-30 23:22:55 +0800

    test: wire multi-Widget flatten RED

--- tests/test_pdf_flatten_main.c
+++ tests/test_pdf_flatten_main.c
@@ -1,6 +1,7 @@
 #include "test_pdf_flatten_internal.h"
 
 int extractpdf_pdf_flatten_base_main(void);
+int extractpdf_test_pdf_flatten_form_multi(void);
 
 int main(void)
 {
@@ -9,6 +10,8 @@ int main(void)
     if (extractpdf_test_pdf_flatten_raw() != 0)
         return 1;
     if (extractpdf_test_pdf_flatten_form() != 0)
+        return 1;
+    if (extractpdf_test_pdf_flatten_form_multi() != 0)
         return 1;
     return extractpdf_pdf_flatten_base_main();
 }
