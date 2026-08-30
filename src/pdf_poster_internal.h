3ae1062e381898e7c3c956de9d2391fd7d9293d4
Qi Gao <qi.gao2012@gmail.com>
2026-08-30 11:54:00 +0800

    refactor: expose Poster Widget provenance preflight

--- src/pdf_poster_internal.h
+++ src/pdf_poster_internal.h
@@ -80,6 +80,11 @@ extractpdf_status extractpdf_pdf_poster_annotations_preflight(
     pdf_document *document,
     extractpdf_pdf_poster_plan *plan);
 
+extractpdf_status extractpdf_pdf_poster_widget_provenance_preflight(
+    fz_context *ctx,
+    pdf_document *document,
+    extractpdf_pdf_poster_plan *plan);
+
 void extractpdf_pdf_poster_drop_annotation_plans(
     extractpdf_pdf_poster_plan *plan);
 
