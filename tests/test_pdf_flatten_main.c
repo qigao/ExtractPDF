01d17477f1849482ee17dc23ddb568866e5ea81b
Qi Gao <qi.gao2012@gmail.com>
2026-08-31 10:03:57 +0800

    test: run generalized AcroForm closure RED

--- tests/test_pdf_flatten_main.c
+++ tests/test_pdf_flatten_main.c
@@ -4,6 +4,7 @@ int extractpdf_pdf_flatten_base_main(void);
 int extractpdf_test_pdf_flatten_form_multi(void);
 int extractpdf_test_pdf_flatten_form_closure(void);
 int extractpdf_test_pdf_flatten_form_ancestor_survives(void);
+int extractpdf_test_pdf_flatten_form_general(void);
 int extractpdf_test_pdf_flatten_widget_as(void);
 int extractpdf_test_pdf_flatten_policy(void);
 int extractpdf_test_pdf_flatten_determinism(void);
@@ -23,6 +24,8 @@ int main(void)
     if (extractpdf_test_pdf_flatten_form_closure() != 0)
         return 1;
     if (extractpdf_test_pdf_flatten_form_ancestor_survives() != 0)
+        return 1;
+    if (extractpdf_test_pdf_flatten_form_general() != 0)
         return 1;
     if (extractpdf_test_pdf_flatten_widget_as() != 0)
         return 1;
