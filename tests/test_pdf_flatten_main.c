710abc801a986fa48287d9c97448a6efca0a4334
Qi Gao <qi.gao2012@gmail.com>
2026-08-31 09:02:45 +0800

    test: run combined Flatten composition RED

--- tests/test_pdf_flatten_main.c
+++ tests/test_pdf_flatten_main.c
@@ -6,6 +6,7 @@ int extractpdf_test_pdf_flatten_form_closure(void);
 int extractpdf_test_pdf_flatten_form_ancestor_survives(void);
 int extractpdf_test_pdf_flatten_widget_as(void);
 int extractpdf_test_pdf_flatten_policy(void);
+int extractpdf_test_pdf_flatten_combined(void);
 
 int main(void)
 {
@@ -24,6 +25,8 @@ int main(void)
     if (extractpdf_test_pdf_flatten_widget_as() != 0)
         return 1;
     if (extractpdf_test_pdf_flatten_policy() != 0)
+        return 1;
+    if (extractpdf_test_pdf_flatten_combined() != 0)
         return 1;
     return extractpdf_pdf_flatten_base_main();
 }
